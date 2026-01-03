0fe72e52a807f7236daed8be18a02af51cdac740
Amarta113 <amartawaghani@gmail.com>
2026-01-03 22:01:37 +0500

    chore: update server configuration and startup logic

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
-const app = require("./app");
+import "dotenv/config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import express from "express"
 
-process.on("uncaughtException", (err) => {
-    console.log(`Error: ${err.message}`)
-    console.log(`Shutting down the server for handling uncaught exception`)
-})
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credential: true
+}))
+
+app.get('/', (req, res) => {res.json({success: true, message: "server is running"})})
 
 
-if(process.env.NODE_ENV !== 'PRODUCTION'){
-    require("dotenv").config({
-        path:"backend/config/.env"
-    })
-}
 
-
-const server = app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`)
+const PORT = process.env.PORT || 8000
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
 
 process.on("unhandledRejection", (err) => {
@@ -25,5 +29,4 @@ process.on("unhandledRejection", (err) => {
     })
 })
 
-module.exports = server;
 
