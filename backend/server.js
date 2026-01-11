3b9af2497249d9ea5f70e6c96ed0388f9d6969da
Amarta113 <amartawaghani@gmail.com>
2026-01-11 21:30:12 +0500

    added user route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import express from "express"
 import { errorMiddleware } from "./middleware/error.js";
+import userRouter from "./routes/userRouter.js"
 
 const app = express()
 
@@ -12,6 +13,7 @@ app.use(cors({
     origin: process.env.FRONTEND_URL,
     credential: true
 }))
+app.use('/api/v1/user', userRouter)
 app.get('/', (req, res) => {res.json({success: true, message: "server is running"})})
 app.use(errorMiddleware)
 
