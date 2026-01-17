3aba6dd9ca3707f1adaa0cb0b77333638d395a9d
Amarta113 <amartawaghani@gmail.com>
2026-01-17 21:41:19 +0500

    added register route

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import { register } from '../controller/userController.js';
+import { upload } from '../multer.js';
 
 const router = express.Router()
-router.post("/register", register)
+router.post("/register", upload.single("avatar"), register)
 
 export default router;
