de9aae8edf2a3bc2237d97c84f2d4d850b163ff4
Amarta113 <amartawaghani@gmail.com>
2026-01-18 21:41:20 +0500

    fixed env config and file export

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,8 +1,13 @@
+import dotenv from 'dotenv';
 import pkg from 'cloudinary'
 const {v2: cloudinary} = pkg
 
-export default cloudinary.config({
+dotenv.config({path: "../.env"})
+
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
+
+export default cloudinary;
