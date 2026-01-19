d96b5c26efabd3f88c5d71c0a0796a8ee260bb5e
Amarta113 <amartawaghani@gmail.com>
2026-01-19 18:05:24 +0500

    fixed upload import

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import { register } from '../controller/userController.js';
-import { upload } from '../multer.js';
+import upload from '../multer.js';
 
 const router = express.Router()
-router.post("/register", upload.single("avatar"), register)
+router.post("/create-user", upload.single("file"), register)
 
 export default router;
