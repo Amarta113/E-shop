f4fb5fe2edd155eb52b0e1c75cf0b070d0e7328e
Amarta113 <amartawaghani@gmail.com>
2026-02-07 12:15:59 +0500

    remove duplicated

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,1 @@
-const express = require('express')
 
-const app = express()
-
-if(process.env.NODE_ENV !== 'PRODUCTION'){
-    require("dotenv").config({
-        path:"backend/config/.env"
-    })
-}
-
-module.exports = app;
