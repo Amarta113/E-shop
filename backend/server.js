cece96c113fbae84c47704fcd18575b29c555d23
Amarta113 <amartawaghani@gmail.com>
2026-02-09 17:57:20 +0500

    fixed typo

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     origin: process.env.FRONTEND_URL,
-    credential: true
+    credentials: true
 }))
 app.use('/api/v1/user', userRouter)
 app.get('/', (req, res) => {res.json({success: true, message: "server is running"})})
