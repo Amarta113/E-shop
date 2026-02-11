8deb12803f6252e6b867026982b84ebc48c619a2
Amarta113 <amartawaghani@gmail.com>
2026-02-11 13:47:08 +0500

    change verify method to jwt tokens

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,8 +10,6 @@ const userSchema = new mongoose.Schema({
         maxLength: [32, "Password cannot have more than 32 characters."]
     },
     accountVerified: {type: Boolean, default: false},
-    verificationCode: Number,
-    verificationCodeExpire: Date,
     resetPasswordToken: String,
     resetPasswordExpire: Date,
     createdAt: {
@@ -30,32 +28,23 @@ const userSchema = new mongoose.Schema({
     }, 
 })
 
-userSchema.pre("save", async function(next){
+userSchema.pre("save", async function (next){
     if(!this.isModified("password")){
         next()
     }
     this.password = await bcrypt.hash(this.password, 10)
 })
 
+// jwt token
+userSchema.methods.getJwtToken = function(){
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+    })
+}
+
 userSchema.methods.comparePassword = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password);
 }
 
-userSchema.methods.generateVerificationCode = function () {
-  function generateRandomFiveDigitNumber() {
-    const firstDigit = Math.floor(Math.random() * 9) + 1;
-    const remainingDigits = Math.floor(Math.random() * 10000)
-      .toString()
-      .padStart(4, 0);
-
-    return parseInt(firstDigit + remainingDigits);
-  }
-  const verificationCode = generateRandomFiveDigitNumber();
-  this.verificationCode = verificationCode;
-  // check point: verification code will be expire in 10 mints
-  this.verificationCodeExpire = Date.now() + 10 * 60 * 1000;
-
-  return verificationCode;
-};
 
 export const User = mongoose.model("User", userSchema)
