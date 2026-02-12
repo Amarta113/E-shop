012369729cdcfdaf249ec5d36b5519588aa2694c
Amarta113 <amartawaghani@gmail.com>
2026-02-12 22:01:05 +0500

    added activation route

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { register } from '../controller/userController.js';
 import upload from '../multer.js';
+import { catchAsyncError } from '../middleware/catchAsyncError.js';
 
 const router = express.Router()
 router.post("/create-user", upload.single("file"), register)
+router.post("/activation", activateUser)
 
 export default router;
