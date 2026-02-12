84f7a1eeb5f755cd7ef2bcb6c4fc25ef519484b7
Amarta113 <amartawaghani@gmail.com>
2026-02-12 22:06:34 +0500

    setup tokens function

---
+++ backend/config/sendTokens.js
@@ -0,0 +1,16 @@
+export const sendTokens = (user, statusCode, res) => {
+    const token = user.getJwtToken()
+
+    const options = {
+        expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        sameSite: "none",
+        secure: true
+    }
+
+    return res.status(statusCode).cookie("token", token, options).json({
+        success: true,
+        user,
+        token
+    })
+}
