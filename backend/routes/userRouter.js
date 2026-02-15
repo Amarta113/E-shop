04de3f171cd206eb944cf29dc1bafa499b597b57
Amarta113 <amartawaghani@gmail.com>
2026-02-15 20:29:20 +0500

    setup user login route

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { register, activateUser } from '../controller/userController.js';
+import { register, activateUser, loginUser } from '../controller/userController.js';
 import upload from '../multer.js';
 import { catchAsyncError } from '../middleware/catchAsyncError.js';
 
 const router = express.Router()
 router.post("/create-user", upload.single("file"), register)
 router.post("/activation", activateUser)
+router.post("/login-user", loginUser);
 
 export default router;
