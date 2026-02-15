a3868be75853d29b6a2470bdd1ce5d31d88e6854
Amarta113 <amartawaghani@gmail.com>
2026-02-15 18:45:14 +0500

    added toast conatiner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import {LoginPage, SignupPage, ActivationPage} from "./Routes.jsx"
+import { ToastContainer } from 'react-toastify';
+
 
 export default function App () {
 
@@ -11,7 +13,21 @@ export default function App () {
         <Route path='/sign-up' element={<SignupPage/>} />
         <Route path='/activation/:activation_token' element={<ActivationPage/>} />
       </Routes>
+      <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          transition={Bounce}
+          />
     </BrowserRouter>
+    
   )
 }
 
