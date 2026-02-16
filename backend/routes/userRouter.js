3a4622af6c5c516a3a49c76d1b9fb2939977a1b1
Amarta113 <amartawaghani@gmail.com>
2026-02-16 12:48:20 +0500

    added get user route

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { register, activateUser, loginUser } from '../controller/userController.js';
+import { register, activateUser, loginUser, loadUser} from '../controller/userController.js';
 import upload from '../multer.js';
 import { catchAsyncError } from '../middleware/catchAsyncError.js';
+import { isAuthenticated } from '../middleware/auth.js';
 
 const router = express.Router()
 router.post("/create-user", upload.single("file"), register)
 router.post("/activation", activateUser)
 router.post("/login-user", loginUser);
+router.get("/get-user", isAuthenticated, loadUser)
 
 export default router;
