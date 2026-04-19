29b9b36243cb857e42cff48174cb5b232720ceb9
Amarta113 <amartawaghani@gmail.com>
2026-04-19 18:39:17 +0500

    added hompage

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,9 +1,11 @@
 import LoginPage from './pages/LoginPage.jsx'
 import SignupPage from './pages/SignupPage.jsx';
 import ActivationPage from './pages/ActivationPage.jsx';
+import HomePage from '../src/pages/HomePage.jsx';
 
 export {
     LoginPage,
     SignupPage,
-    ActivationPage
+    ActivationPage,
+    HomePage
 }
