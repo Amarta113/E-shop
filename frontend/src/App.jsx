53b35b10c72684f4e494752bfdcf4b02f790e31f
Amarta113 <amartawaghani@gmail.com>
2026-04-19 18:39:06 +0500

    added redux dispatcher in useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,21 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import {LoginPage, SignupPage, ActivationPage} from "./Routes.jsx"
+import {LoginPage, SignupPage, ActivationPage, HomePage} from "./Routes.jsx"
 import { toast, ToastContainer, Bounce } from 'react-toastify';
 import { useEffect } from 'react';
 import axios from 'axios';
+import store from '../redux/store.js';
+import { loadUser } from '../redux/actions/user.js';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default function App () {
   useEffect(() => {
-    axios.get(`${API_BASE_URL}/user/get-user`, {withCredentials: true})
-    .then((res) => {
-      toast.success(res.data.message)
-    })
-    .catch((err) => {
-      toast.error(err?.response?.data?.message);
-    })
+   store.dispatch(loadUser())
   }, [])
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/Home' element={<HomePage/>} />
         <Route path='/login' element={<LoginPage/>} />
         <Route path='/sign-up' element={<SignupPage/>} />
         <Route path='/activation/:activation_token' element={<ActivationPage/>} />
