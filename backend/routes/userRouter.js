602565937b8a45db158cb52e06c1f6be752d6c9b
Amarta113 <amartawaghani@gmail.com>
2026-04-19 18:35:38 +0500

    fix file name and api name

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -5,7 +5,7 @@ import { catchAsyncError } from '../middleware/catchAsyncError.js';
 import { isAuthenticated } from '../middleware/auth.js';
 
 const router = express.Router()
-router.post("/create-user", upload.single("file"), register)
+router.post("/register", upload.single("avatar"), register)
 router.post("/activation", activateUser)
 router.post("/login-user", loginUser);
 router.get("/get-user", isAuthenticated, loadUser)
