6a4ae3e5c88fb1651d753f7a3fb429de9da4d138
Amarta113 <amartawaghani@gmail.com>
2026-04-25 21:37:46 +0500

    replace logo with Header component in HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,10 @@
 import { Link } from 'react-router-dom'
+import Header from '../components/Layout/Header.jsx'
 
 export default function HomePage () {
     return (
-        <div className='hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between'>
-            <div>
-                <Link to="/">
-                    <img src="C:\Users\hp\Downloads\Vendorz_logo.png" alt='logo'/>
-                </Link>
-            </div>
+        <div>
+            <Header/>            
         </div>
     )
 }
