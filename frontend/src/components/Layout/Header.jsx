869b0b2c29a865947db5a618a30a2b777f2d1a66
Amarta113 <amartawaghani@gmail.com>
2026-04-25 21:36:54 +0500

    added lucide react

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logoImg from '../../assets/vendors-logo.png'
+import styles from '../../styles/styles'
 
 export default function Header() {
     return (
-        <div>
-            <h1>Header</h1>
+        <>
+        <div className={`${styles.section}`}>
+            <div className="h-[50px] my-[20px] flex items-center justify-between">
+                <div>
+                    <Link to='/'>
+                        <img src={logoImg} alt='logo' className='w-[40px]' />
+                    </Link>
+                </div>
+            </div>
         </div>
+        </>
     )
 }
