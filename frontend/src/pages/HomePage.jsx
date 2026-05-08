6e0817ecc2443e0706db67d12ca0f9c1b4ae6214
Amarta113 <amartawaghani@gmail.com>
2026-05-08 21:45:22 +0500

    added categories component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom'
 import Header from '../components/Layout/Header.jsx'
 import Hero from '../components/Route/Hero/Hero.jsx'
+import Categories from "../components/Route/Categories/Categories.jsx"
 
 export default function HomePage () {
     return (
         <div>
             <Header activeHeading={1}/>
-            <Hero/>            
+            <Hero/> 
+            <Categories/>           
         </div>
     )
 }
