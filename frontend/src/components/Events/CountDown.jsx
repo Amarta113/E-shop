1154b2673f6e597c61e795afbabcf8b592c56170
Amarta113 <amartawaghani@gmail.com>
2026-05-11 21:37:46 +0500

    fixed timer

--- frontend/src/components/Events/CountDown.jsx
+++ frontend/src/components/Events/CountDown.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 
 const calculateTimeLeft = () => {
-  const difference = +new Date("2026-12-31T23:59:59") - +new Date();
-
-  if (difference <= 0) return {};
-
-  return {
-    days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-    hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-    minutes: Math.floor((difference / (1000 * 60)) % 60),
-    seconds: Math.floor((difference / 1000) % 60),
+  const difference = +new Date("2026-5-19") - +new Date();
+  let timeLeft = {};
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / (1000 * 60)) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    }
   };
+
+  return timeLeft;
 };
 
 export default function CountDown() {
@@ -22,7 +24,7 @@ export default function CountDown() {
     }, 1000);
 
     return () => clearInterval(timer);
-  }, []);
+  });
 
   const timerComponents = Object.keys(timeLeft).map((interval) => (
     <span key={interval} className="text-[25px] text-[#475ad2] mr-2">
