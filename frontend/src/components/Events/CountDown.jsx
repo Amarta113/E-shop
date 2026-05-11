78c4134a627e862455333854a7cbe880ff7123e3
Amarta113 <amartawaghani@gmail.com>
2026-05-11 19:36:46 +0500

    added CountDown component to display a countdown timer until the specified date

---
+++ frontend/src/components/Events/CountDown.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+const calculateTimeLeft = () => {
+  const difference = +new Date("2026-12-31T23:59:59") - +new Date();
+
+  if (difference <= 0) return {};
+
+  return {
+    days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+    hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+    minutes: Math.floor((difference / (1000 * 60)) % 60),
+    seconds: Math.floor((difference / 1000) % 60),
+  };
+};
+
+export default function CountDown() {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const timerComponents = Object.keys(timeLeft).map((interval) => (
+    <span key={interval} className="text-[25px] text-[#475ad2] mr-2">
+      {timeLeft[interval]} {interval}
+    </span>
+  ));
+
+  return (
+    <div>
+      {timerComponents.length ? (
+        timerComponents
+      ) : (
+        <span className="text-[red] text-[25px]">Time&apos;s Up!</span>
+      )}
+    </div>
+  );
+}
