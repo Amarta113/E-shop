595d88179b0415ecb6c95d5d965f6030b9c8e044
Amarta113 <amartawaghani@gmail.com>
2026-05-11 19:35:51 +0500

    added BestDeals, FeaturedProduct, and Events components to HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,13 +2,19 @@ import { Link } from 'react-router-dom'
 import Header from '../components/Layout/Header.jsx'
 import Hero from '../components/Route/Hero/Hero.jsx'
 import Categories from "../components/Route/Categories/Categories.jsx"
+import BestDeals from '../components/Route/BestDeals.jsx'
+import FeaturedProduct from '../components/Route/FeaturedProduct.jsx'
+import Events from '../components/Events/Events.jsx'
 
 export default function HomePage () {
     return (
         <div>
             <Header activeHeading={1}/>
             <Hero/> 
-            <Categories/>           
+            <Categories/>   
+            <BestDeals/> 
+            <Events/>
+            <FeaturedProduct/>       
         </div>
     )
 }
