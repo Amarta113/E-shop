043a1d2e8ac2d0cf6bdc9c9603c7e4ac23af16a3
Amarta113 <amartawaghani@gmail.com>
2026-05-13 20:51:03 +0500

    added multiple new page components (ProductsPage, BestSellingPage, EventsPage, FaqPage) to Routes for enhanced site navigation and content display

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -2,10 +2,19 @@ import LoginPage from './pages/LoginPage.jsx'
 import SignupPage from './pages/SignupPage.jsx';
 import ActivationPage from './pages/ActivationPage.jsx';
 import HomePage from '../src/pages/HomePage.jsx';
+import ProductsPage from '../src/pages/ProductsPage.jsx'
+import BestSellingPage from '../src/pages/BestSellingPage.jsx'
+import EventsPage from '../src/pages/EventsPage'
+import FaqPage from '../src/pages/FaqPage.jsx'
+
 
 export {
     LoginPage,
     SignupPage,
     ActivationPage,
-    HomePage
+    HomePage,
+    ProductsPage,
+    BestSellingPage,
+    EventsPage,
+    FaqPage
 }
