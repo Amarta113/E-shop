18a59aca96a353037d446eceda0908c4ef8c37c6
Amarta113 <amartawaghani@gmail.com>
2026-05-13 20:50:08 +0500

    added EventsPage component to display events with a header and event card

---
+++ frontend/src/pages/EventsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Layout/Header'
+import EventCard from '../components/Events/EventCard'
+
+export default function EventsPage() {
+    return (
+        <div>
+            <Header activeHeading={4} />
+            <EventCard active={true}/>
+        </div>
+    )
+}
+
