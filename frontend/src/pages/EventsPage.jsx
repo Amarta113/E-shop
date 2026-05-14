a792210379258e2b6dc42af8b19e0528f9262c1e
Amarta113 <amartawaghani@gmail.com>
2026-05-14 21:45:37 +0500

    added Footer component and wrapped EventCard in a main section for improved layout on EventsPage

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../components/Layout/Header'
+import Footer from '../components/Layout/Footer'
 import EventCard from '../components/Events/EventCard'
+import styles from '../styles/styles'
 
 export default function EventsPage() {
     return (
         <div>
             <Header activeHeading={4} />
-            <EventCard active={true}/>
+            <main className={`${styles.section} max-w-6xl py-10 pb-16 px-4 sm:px-0`}>
+            <EventCard active={true} />
+            </main>
+            <Footer />
         </div>
     )
 }
